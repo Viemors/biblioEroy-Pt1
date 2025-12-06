4615a710096a7203add616d835ca6ffe57429e2b
Isabelli <isabelli.nobrega@aluno.ifsp.edu.br>
2025-12-06 19:47:53 -0300

    todos os css modificados, +imgs, transicoes do home, css da tabela, ejs organizados

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,18 +1,17 @@
 const sections = document.querySelectorAll("section");
 
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("active");
-      } else {
-        entry.target.classList.remove("active");
-      }
-    });
-  },
-  {
-    threshold: 0.3,
-  }
-);
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+}, { threshold: 0.3 });
 
-sections.forEach((section) => observer.observe(section));
+sections.forEach(sec => observer.observe(sec));
+
+window.addEventListener("load", () => {
+  document.body.classList.add("mostrar");
+});
+
+//comentar
