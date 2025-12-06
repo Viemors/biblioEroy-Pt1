9e05a76b816aaac6af952d019e79c81f66ed2e7f
Isabelli <isabelli.nobrega@aluno.ifsp.edu.br>
2025-12-05 22:21:29 -0300

    + um css, ejs, um pouco de transições

---
+++ src/public/js/home.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      } else {
+        entry.target.classList.remove("active");
+      }
+    });
+  },
+  {
+    threshold: 0.3,
+  }
+);
+
+sections.forEach((section) => observer.observe(section));
